804191e757d4a5709871825c5a584e262402f14d
atuning120 <90119017+atuning120@users.noreply.github.com>
2025-09-05 10:56:43 -0400

    probar endpoints del usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Login from './pages/login.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hola Mundo</h1>
+    <div className="App">
+      <Login />
+    </div>
   )
 }
 
